a637ecefc7067f8771f25d3258b0df7a00d6ca64
Barak Tarab <baraktarab@gmail.com>
2021-12-09 16:36:08 +0200

    - Start tests

--- src/test/api/BaseNodeDataTest.java
+++ src/test/api/BaseNodeDataTest.java
@@ -6,24 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BaseNodeDataTest {
 
+    BaseNodeData n1 = new BaseNodeData(10, 5, "Bla", 0.555, new BaseGeoLocation(1,2,0));
+
     @Test
     void getKey() {
+        assertEquals(10, n1.getKey());
     }
 
     @Test
     void getLocation() {
+        GeoLocation g = n1.getLocation();
+        assertEquals(1, g.x());
+        assertEquals(2, g.y());
+        assertEquals(0, g.z());
     }
 
     @Test
     void setLocation() {
+        BaseNodeData n2 = new BaseNodeData(10, 5, "Bla", 0.555, new BaseGeoLocation(1,2,0));
+        n2.setLocation(new BaseGeoLocation(4,5,6));
+
+        GeoLocation g = n2.getLocation();
+        assertEquals(4, g.x());
+        assertEquals(5, g.y());
+        assertEquals(6, g.z());
     }
 
     @Test
     void getWeight() {
+        assertEquals(0.555, n1.getWeight());
     }
 
     @Test
     void setWeight() {
+        BaseNodeData n2 = new BaseNodeData(10, 5, "Bla", 0.555, new BaseGeoLocation(1,2,0));
+        n2.setWeight(24);
+        assertEquals(24, n2.getWeight());
     }
 
     @Test
