cca206a3fe2329c942b2d035e762fcc4271b5767
MoriahDavid <moriah.david24@gmail.com>
2021-12-09 18:39:10 +0200

    Test for BaseDirectedWeightedGraphTest

--- src/test/api/BaseDirectedWeightedGraphTest.java
+++ src/test/api/BaseDirectedWeightedGraphTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BaseDirectedWeightedGraphTest {
 
-    BaseDirectedWeightedGraph getGraph() {
+    BaseDirectedWeightedGraph getNewGraph() {
         BaseDirectedWeightedGraph g = new BaseDirectedWeightedGraph();
         g.addNode(new BaseNodeData(1,0,"", 0, new BaseGeoLocation(0,0,0)));
         g.addNode(new BaseNodeData(2,0,"", 0, new BaseGeoLocation(0,0,0)));
@@ -16,7 +16,7 @@ class BaseDirectedWeightedGraphTest {
 
     @Test
     void transpose() {
-        BaseDirectedWeightedGraph g = getGraph();
+        BaseDirectedWeightedGraph g = getNewGraph();
         assertNotNull(g.getEdge(1,2));
         assertNull(g.getEdge(2,1));
         g.transpose();
@@ -26,7 +26,7 @@ class BaseDirectedWeightedGraphTest {
 
     @Test
     void getNode() {
-        BaseDirectedWeightedGraph g = getGraph();
+        BaseDirectedWeightedGraph g = getNewGraph();
         assertNotNull(g.getNode(1));
         assertInstanceOf(BaseNodeData.class, g.getNode(1));
         assertNull(g.getNode(3));
@@ -34,45 +34,62 @@ class BaseDirectedWeightedGraphTest {
 
     @Test
     void getEdge() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertNotNull(g.getEdge(1,2));
+        assertNull(g.getEdge(2,1));
     }
 
     @Test
     void addNode() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertNull(g.getNode(45));
+        g.addNode(new BaseNodeData(45, 0, "", 22, new BaseGeoLocation(10,20,0)));
+        assertNotNull(g.getNode(45));
     }
 
     @Test
     void connect() {
-    }
-
-    @Test
-    void nodeIter() {
-    }
-
-    @Test
-    void edgeIter() {
-    }
-
-    @Test
-    void testEdgeIter() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertNull(g.getEdge(2, 1));
+        g.connect(2,1,80.0);
+        assertNotNull(g.getEdge(2, 1));
     }
 
     @Test
     void removeNode() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertNotNull(g.getNode(1));
+        assertNotNull(g.getEdge(1, 2));
+        g.removeNode(1);
+        assertNull(g.getNode(1));
+        assertNull(g.getEdge(1, 2));
     }
 
     @Test
     void removeEdge() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertNotNull(g.getNode(1));
+        assertNotNull(g.getEdge(1, 2));
+        g.removeEdge(1, 2);
+        assertNotNull(g.getNode(1));
+        assertNull(g.getEdge(1, 2));
     }
 
     @Test
     void nodeSize() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertEquals(2, g.nodeSize());
     }
 
     @Test
     void edgeSize() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertEquals(1, g.edgeSize());
     }
 
     @Test
     void getMC() {
+        BaseDirectedWeightedGraph g = getNewGraph();
+        assertEquals(3, g.getMC());
     }
 }
